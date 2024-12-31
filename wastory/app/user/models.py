1d905a022ca3d611490a644a0584feb7c9ab8282
SooinPark1019 <hyperion1019@DESKTOP-0K9R2DU>
2024-12-31 17:22:32 +0900

    server test good

--- wastory/app/user/models.py
+++ wastory/app/user/models.py
@@ -1,12 +1,12 @@
-from sqlalchemy import BigInteger, String
-from sqlalchemy.orm import Mapped, mapped_column
-from wastory.database.common import Base
-
+from typing import TYPE_CHECKING
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from wastory.database.common import Base, intpk
 
 class User(Base):
     __tablename__ = "user"
 
-    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    id: Mapped[intpk]
     username: Mapped[str] = mapped_column(String(20), unique=True, index=True)
     email: Mapped[str] = mapped_column(String(100), unique=True, index=True)
     password: Mapped[str] = mapped_column(String(20))
