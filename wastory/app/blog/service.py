4cae133c4749ba275ffa80deb20fd3c8cdfc836b
SooinPark1019 <hyperion1019@DESKTOP-0K9R2DU>
2025-01-07 00:33:53 +0900

    blog/request 작성

--- wastory/app/blog/service.py
+++ wastory/app/blog/service.py
@@ -1,16 +1,24 @@
 from typing import Annotated
 
 from fastapi import Depends
+from wastory.app.blog.models import Blog
 from wastory.app.user.models import User
-from wastory.app.user.store import UserStore
+from wastory.app.blog.store import BlogStore
 
 
 class UserService:
-    def __init__(self, user_store: Annotated[UserStore, Depends()]) -> None:
-        self.user_store = user_store
+    def __init__(self, blog_store: Annotated[BlogStore, Depends()]) -> None:
+        self.blog_store = blog_store
+
+    async def create_blog(
+        self,
+        user : User,
+        name : str,
+        description : str
+    ) -> 
 
     async def add_user(self, username: str, password: str, email: str):
-        await self.user_store.add_user(username=username, password=password, email=email)
+        await self.blog_store.add_blog(username=username, password=password, email=email)
 
     async def get_user_by_username(self, username: str) -> User | None:
         return await self.user_store.get_user_by_username(username)
