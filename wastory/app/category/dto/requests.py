22954b95958cf3907896b4f5a571179507ef9077
Ubuntu <ubuntu@ip-172-31-39-253.ap-northeast-2.compute.internal>
2025-01-22 14:43:15 +0000

    category 이름 중복 해결

--- wastory/app/category/dto/requests.py
+++ wastory/app/category/dto/requests.py
@@ -20,15 +20,14 @@ class CategoryCreateRequest(BaseModel):
     categoryname: Annotated[str,AfterValidator(validate_categoryname)]
     categoryLevel: Annotated[int, AfterValidator(validate_categorylevel)]
     parent_id: int|None=None
-    level: int
 
     @model_validator(mode="after")
     def validate_level_and_parent_id(self) -> "CategoryCreateRequest":
-        if self.level not in (1, 2):  # level 값이 1 또는 2가 아닐 경우 에러 처리
+        if self.categoryLevel not in (1, 2):  # level 값이 1 또는 2가 아닐 경우 에러 처리
             raise InvalidFieldFormatError()
-        if self.level == 2 and self.parent_id is None:
+        if self.categoryLevel == 2 and self.parent_id is None:
             raise InvalidFieldFormatError()
-        if self.level == 1 and self.parent_id is not None:
+        if self.categoryLevel == 1 and self.parent_id is not None:
             raise InvalidFieldFormatError()
         return self
 
