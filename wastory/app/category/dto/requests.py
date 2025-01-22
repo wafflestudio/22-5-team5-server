830c427f98720ebb3bc9669a42ca356e78cb14b7
jaylions <jaylions@snu.ac.kr>
2025-01-22 23:05:09 +0900

    category error handle

--- wastory/app/category/dto/requests.py
+++ wastory/app/category/dto/requests.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from pydantic import BaseModel
-from pydantic.functional_validators import AfterValidator
+from pydantic.functional_validators import AfterValidator,model_validator
 
 from wastory.app.category.errors import InvalidFieldFormatError
 
@@ -20,6 +20,17 @@ class CategoryCreateRequest(BaseModel):
     categoryname: Annotated[str,AfterValidator(validate_categoryname)]
     categoryLevel: Annotated[int, AfterValidator(validate_categorylevel)]
     parent_id: int|None=None
+    level: int
+
+    @model_validator(mode="after")
+    def validate_level_and_parent_id(self) -> "CategoryCreateRequest":
+        if self.level not in (1, 2):  # level 값이 1 또는 2가 아닐 경우 에러 처리
+            raise InvalidFieldFormatError()
+        if self.level == 2 and self.parent_id is None:
+            raise InvalidFieldFormatError()
+        if self.level == 1 and self.parent_id is not None:
+            raise InvalidFieldFormatError()
+        return self
 
 class CategoryUpdateRequest(BaseModel):
     categoryname: Annotated[str,AfterValidator(validate_categoryname)]
