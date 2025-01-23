43c7518db48a500914f24ea87cf3c01b82ce90a0
jaylions <jaylions@snu.ac.kr>
2025-01-23 11:48:02 +0900

    category list response test 전임

--- wastory/app/category/service.py
+++ wastory/app/category/service.py
@@ -7,6 +7,7 @@ from wastory.app.category.errors import BlogNotFoundError
 from wastory.app.user.models import User
 from wastory.app.user.store import UserStore
 from wastory.app.blog.store import BlogStore
+from wastory.app.category.models import Category
 class CategoryService:
     def __init__(
         self, 
@@ -45,32 +46,52 @@ class CategoryService:
     )->None:
         await self.category_store.delete_category(user, category_id)
 
-    async def list_categories(
-        self,
-        user:User
-    )->CategoryFinalResponse:
-        categories=await self.category_store.get_category_of_blog(user.blogs.id)
-        category_list= [
-            CategoryListResponse.from_category(
-                category,
-                await self.category_store.get_article_count(category.id)
-                ) 
-                for category in categories
-            ]
-        return CategoryFinalResponse.from_categorylist(category_list)
-    
-    async def list_categories_by_blog(
-        self,
-        blog_id:int
-    )->CategoryFinalResponse:
-        categories=await self.category_store.get_category_of_blog(blog_id)
-        category_list= [
-            CategoryListResponse.from_category(
-                category,
-                await self.category_store.get_article_count(category.id)
-                ) 
-                for category in categories
-            ]
-        return CategoryFinalResponse.from_categorylist(category_list)
+    async def list_categories(self, user: User) -> CategoryFinalResponse:
+    # level=1 카테고리만 가져오되, 자식( level=2 )을 Eager Loading
+        categories = await self.category_store.get_category_of_blog(user.blogs.id)
+        
+        category_list_responses = []
+        for cat in categories:
+            # 상위 카테고리의 article_count
+            parent_article_count = await self.comment_store.get_article_count(cat.id)
+            parent_resp = CategoryListResponse.from_category(cat, parent_article_count)
+
+            # 자식 카테고리( level=2 ) 처리
+            child_resp_list = []
+            for child_cat in cat.children:
+                child_article_count = await self.comment_store.get_article_count(child_cat.id)
+                child_resp = CategoryListResponse.from_category(child_cat, child_article_count)
+                child_resp_list.append(child_resp)
+
+            # 상위 카테고리에 자식 리스트 할당
+            parent_resp.children = child_resp_list
+            
+            category_list_responses.append(parent_resp)
+
+        return CategoryFinalResponse.from_categorylist(category_list_responses)
+
+    async def list_categories_by_blog(self, blog_id: int) -> CategoryFinalResponse:
+    # level=1 카테고리만 가져오되, 자식( level=2 )을 Eager Loading
+        categories = await self.category_store.get_category_of_blog(blog_id)
+        
+        category_list_responses = []
+        for cat in categories:
+            # 상위 카테고리의 article_count
+            parent_article_count = await self.comment_store.get_article_count(cat.id)
+            parent_resp = CategoryListResponse.from_category(cat, parent_article_count)
+
+            # 자식 카테고리( level=2 ) 처리
+            child_resp_list = []
+            for child_cat in cat.children:
+                child_article_count = await self.comment_store.get_article_count(child_cat.id)
+                child_resp = CategoryListResponse.from_category(child_cat, child_article_count)
+                child_resp_list.append(child_resp)
+
+            # 상위 카테고리에 자식 리스트 할당
+            parent_resp.children = child_resp_list
+            
+            category_list_responses.append(parent_resp)
+
+        return CategoryFinalResponse.from_categorylist(category_list_responses)
 
 
